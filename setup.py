290d485daa318c9fb0227fbe36bf4f1324865427
Brian Bockelman <bbockelm@cse.unl.edu>
2015-04-20 14:46:20 -0500

    Add new launch scripts to RPM.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(name='kestrel',
       version='0.2',
-      scripts=['src/gfactory_entry_requests', 'src/gfactory_frontend_groups', 'src/gfactory_frontend_info', 'src/gwms_analyze_job', 'src/kestrel_config_val', 'src/kestrel_unzip_log', 'src/gwms_compare_collectors'],
+      scripts=['src/gfactory_entry_requests', 'src/gfactory_frontend_groups', 'src/gfactory_frontend_info', 'src/gwms_analyze_job', 'src/kestrel_config_val', 'src/kestrel_unzip_log', 'src/gwms_compare_collectors', 'src/kestrel_launch_pilot', 'src/kestrel_pilot_config''],
       data_files=[('/usr/share/kestrel', ['config/kestrel_config_global', 'src/gfactory_entry_requests.cpf', 'src/gfactory_frontend_groups.cpf']),
                   ('/etc/kestrel', ['config/kestrel_config']),
                   ('/etc/kestrel/config.d', ['config/01-osg.conf']),
